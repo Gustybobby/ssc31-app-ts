94c890ba4a162ee9147bbae7ea50ab450ce2fce9
Gustybobby <napattatiyakaroonwong@gmail.com>
2024-01-02 19:01:53 +0700

    import set

---
+++ server/set.ts
@@ -0,0 +1,54 @@
+export function setDifference<T>(setA: Set<T>, setB: Set<T>){
+    const setACopy: Set<T> = new Set(setA)
+    setB.forEach((e) => {
+        setACopy.delete(e)
+    })
+    return setACopy
+}
+
+export function setIntersection<T>(setA: Set<T>, setB: Set<T>){
+    const intersect: Set<T> = new Set()
+    setA.forEach((e) => {
+        if(setB.has(e)){
+            intersect.add(e)
+        }
+    })
+    return intersect
+}
+
+export function setUnion<T>(setA: Set<T>, setB: Set<T>){
+    const union: Set<T> = new Set()
+    setA.forEach((e) => {
+        union.add(e)
+    })
+    setB.forEach((e) => {
+        union.add(e)
+    })
+    return union
+}
+
+export function setConnections(newIdList: string[], key: string){
+    return { [key]: newIdList.map((id) => ({ id }))}
+}
+
+export function stringChanges(string: string, prevString: string){
+    const charFreq = getCharFrequency(string)
+    const prevCharFreq = getCharFrequency(prevString)
+    const changeSet: Set<String> = new Set()
+    const allChars = setUnion(new Set(Object.keys(charFreq)), new Set(Object.keys(prevCharFreq)))
+    allChars.forEach((char) => {
+        if((charFreq[char] ?? 0) !== (prevCharFreq[char] ?? 0)){
+            changeSet.add(char)
+        }
+    })
+    return changeSet
+}
+
+function getCharFrequency(string: string){
+    const charFreq: { [key: string]: number } = {}
+    for(const char of string){
+        charFreq[char] = charFreq[char] ?? 0
+        charFreq[char] +=1
+    }
+    return charFreq
+}
