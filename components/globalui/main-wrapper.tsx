578c50f553211eba3ec54999e522943b1dfa56f1
Gustybobby <napattatiyakaroonwong@gmail.com>
2024-01-02 17:32:13 +0700

    import DefaultMain as MainWrapper

--- components/globalui/main-wrapper.tsx
+++ components/globalui/main-wrapper.tsx
@@ -1,4 +1,4 @@
-export default function MainWrapper({ children, extraClass } : { children: React.ReactNode, extraClass: string }){
+export default function MainWrapper({ children, extraClass } : { children: React.ReactNode, extraClass?: string }){
     return(
         <main className="w-full text-black dark:text-white">
             <div className={`flex flex-col items-center h-[92vh] overflow-y-auto ${extraClass}`}>
