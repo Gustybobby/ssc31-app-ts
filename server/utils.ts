864a8adc320de382a4c3e69d839a04ca90241c2e
Gustybobby <napattatiyakaroonwong@gmail.com>
2024-01-03 21:18:11 +0700

    fix searchParamsToSelect bug

--- server/utils.ts
+++ server/utils.ts
@@ -9,5 +9,8 @@ export function searchParamsToSelect(searchParams: URLSearchParams){
     searchParams.forEach((value, key) => {
         select[key] = !!Number(value)
     })
+    if(Object.keys(select).length === 0){
+        return undefined
+    }
     return select
 }
