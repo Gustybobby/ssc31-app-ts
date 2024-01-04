1f23c85dc59b84e66b6547692205eff4c586ddb6
Gustybobby <napattatiyakaroonwong@gmail.com>
2024-01-04 18:58:55 +0700

    remove unused component

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,25 +1,13 @@
 import Image from "next/image"
-export default function LoadingSkeleton(){
-    return(
-        <div className="w-full h-screen flex flex-col items-center pt-60">
-            <div className="flex flex-col items-center justify-center pb-2">
-                <Image src="/static/mixxy.gif" width={160} height={160} alt="Loading"/>
-            </div>
-            <div className="text-center text-black dark:text-white text-xl">
-            <span className="text-3xl font-bold">Mixing</span>
-            </div>
-        </div>
-    )
-}
 
-export function LoadingChild(){
+export default function Loading(){
     return(
         <div className="w-full h-screen flex flex-col items-center pt-60">
             <div className="flex flex-col items-center justify-center pb-2">
                 <Image src="/static/mixxy.gif" width={160} height={160} alt="Loading"/>
             </div>
-            <div className="text-center text-black dark:text-white text-xl">
-                <span className="text-3xl font-bold">Mixing</span>
+            <div className="text-center text-black dark:text-white text-3xl font-bold">
+                Mixing
             </div>
         </div>
     )
