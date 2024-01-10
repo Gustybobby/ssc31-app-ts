3634429b7cf1cef4045eb2b4947b2d32a69f74ca
Gustybobby <napattatiyakaroonwong@gmail.com>
2024-01-10 21:00:11 +0700

    redesign table loading

--- components/tools/gustybobby-tables/gustybobby-table-loading.tsx
+++ components/tools/gustybobby-tables/gustybobby-table-loading.tsx
@@ -1,20 +1,32 @@
 export default function GustybobbyTableLoading(){
     return (
         <div>
-            <div className="flex justify-between space-x-2 w-full h-10 p-2 bg-gray-200 dark:bg-gray-800 border border-black dark:border-white">
-                <div className="w-1/4 h-6 bg-gray-300 dark:bg-gray-700 rounded-lg animate-pulse"/>
-                <div className="w-1/4 h-6 bg-gray-300 dark:bg-gray-700 rounded-lg animate-pulse"/>
-                <div className="w-1/4 h-6 bg-gray-300 dark:bg-gray-700 rounded-lg animate-pulse"/>
-                <div className="w-1/4 h-6 bg-gray-300 dark:bg-gray-700 rounded-lg animate-pulse"/>
+            <div className="grid grid-cols-4 gap-2 w-full h-10 p-2 bg-gray-200 dark:bg-gray-800 border border-black dark:border-white">
+                <div className="h-6 bg-gray-300 dark:bg-gray-700 rounded-lg animate-pulse"/>
+                <div className="h-6 bg-gray-300 dark:bg-gray-700 rounded-lg animate-pulse"/>
+                <div className="h-6 bg-gray-300 dark:bg-gray-700 rounded-lg animate-pulse"/>
+                <div className="h-6 bg-gray-300 dark:bg-gray-700 rounded-lg animate-pulse"/>
             </div>
-            <div className="w-full h-64 p-2 bg-gray-100 dark:bg-gray-700 border border-black dark:border-white space-y-2">
-                <div className="w-full h-8 bg-gray-300 dark:bg-gray-600 rounded-lg animate-pulse"/>
-                <div className="w-full h-8 bg-gray-300 dark:bg-gray-600 rounded-lg animate-pulse"/>
-                <div className="w-full h-8 bg-gray-300 dark:bg-gray-600 rounded-lg animate-pulse"/>
-                <div className="w-full h-8 bg-gray-300 dark:bg-gray-600 rounded-lg animate-pulse"/>
-                <div className="w-full h-8 bg-gray-300 dark:bg-gray-600 rounded-lg animate-pulse"/>
-                <div className="w-full h-8 bg-gray-300 dark:bg-gray-600 rounded-lg animate-pulse"/>
+            <div className="grid grid-cols-4 gap-2 w-full h-64 p-2 bg-gray-100 dark:bg-gray-700 border border-black dark:border-white">
+                <RowCellsLoading/>
+                <RowCellsLoading/>
+                <RowCellsLoading/>
+                <RowCellsLoading/>
+                <RowCellsLoading/>
+                <RowCellsLoading/>
+                <RowCellsLoading/>
             </div>
         </div>
+    )
+}
+
+function RowCellsLoading(){
+    return(
+        <>
+            <div className="h-6 bg-gray-300 dark:bg-gray-600 rounded-lg animate-pulse"/>
+            <div className="h-6 bg-gray-300 dark:bg-gray-600 rounded-lg animate-pulse"/>
+            <div className="h-6 bg-gray-300 dark:bg-gray-600 rounded-lg animate-pulse"/>
+            <div className="h-6 bg-gray-300 dark:bg-gray-600 rounded-lg animate-pulse"/>
+        </>
     )
 }
