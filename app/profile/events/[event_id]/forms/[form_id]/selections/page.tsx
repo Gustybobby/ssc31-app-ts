a4e8e83e9c83c53c71f30d1e6f494c870432368e
Gustybobby <napattatiyakaroonwong@gmail.com>
2024-01-24 15:04:45 +0700

    redirect to profile on unauthenticated

--- app/profile/events/[event_id]/forms/[form_id]/selections/page.tsx
+++ app/profile/events/[event_id]/forms/[form_id]/selections/page.tsx
@@ -1,7 +1,13 @@
+import { getServerAuthSession } from "@/app/api/auth/[...nextauth]/_utils";
 import MainWrapper from "@/components/globalui/main-wrapper";
 import MemberSelections from "@/components/profile/events/event/forms/selections/member-selections";
+import { redirect } from "next/navigation";
 
 export default async function MemberJoinFormMembersSelectionsPage({ params }: { params: { event_id: string, form_id: string }}){
+    const session = await getServerAuthSession()
+    if(!session){
+        redirect('/profile')
+    }
     return (
         <MainWrapper>
             <MemberSelections event_id={params.event_id} form_id={params.form_id}/>
