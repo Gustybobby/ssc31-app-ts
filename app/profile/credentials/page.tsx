a89bbb47306ddb2797cd08dcf152f49434002604
Gustybobby <napattatiyakaroonwong@gmail.com>
2024-02-11 00:50:27 +0700

    change color for easier qr code scanning

--- app/profile/credentials/page.tsx
+++ app/profile/credentials/page.tsx
@@ -12,12 +12,12 @@ export default async function CredentialsPage(){
         <MainWrapper>
             <div className={styles.mainBox}>
                 <div className={styles.sectionBox}>
-                    <div className="w-full md:w-80 bg-gray-100 dark:bg-black/70 p-4 rounded-lg shadow-lg flex flex-col items-center space-y-2">
+                    <div className="w-full md:w-80 bg-black/70 p-4 rounded-lg shadow-lg flex flex-col items-center space-y-2">
                         <div className="border-8 border-red-600 w-fit">
                             <QRCode value={session?.user.id+'</>'+date.toISOString()}/>
                         </div>
                     </div>
-                    <div className="w-full md:w-80 bg-gray-100 dark:bg-black/70 p-4 rounded-lg shadow-lg flex flex-col items-center space-y-2">
+                    <div className="w-full md:w-80 text-white bg-black/70 p-4 rounded-lg shadow-lg flex flex-col items-center space-y-2">
                         <span>Email: {session.user.email}</span>
                         <span>Name: {session.user.name}</span>
                     </div>
@@ -31,11 +31,11 @@ const styles = {
     mainBox:[
         'flex flex-col p-4',
         'w-full h-full shadow-lg',
-        'bg-white/70 dark:bg-black/70'
+        'bg-black/70'
     ].join(' '),
     sectionBox:[
         'p-2 flex flex-col items-center space-y-2',
         'w-full h-full shadow-sm rounded-2xl',
-        'bg-black/10 dark:bg-white/20',
+        'bg-white/20',
     ].join(' ')
 }
