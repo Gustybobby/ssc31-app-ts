fb1331fc7f003093e137b686079446ee9e1283c8
Gustybobby <napattatiyakaroonwong@gmail.com>
2024-02-12 13:33:12 +0700

    handle error in decrypt

--- server/modules/qr-encryption.ts
+++ server/modules/qr-encryption.ts
@@ -1,14 +1,25 @@
 import Cryptr from "cryptr";
 
-if(!process.env.QR_SECRET_KEY){
-    throw 'undefined env variables'
+function initCryptr(){
+    if(!process.env.QR_SECRET_KEY){
+        throw 'undefined env variables'
+    }
+    const cryptr = new Cryptr(process.env.QR_SECRET_KEY, { encoding: 'base64' })
+    return cryptr
 }
-const cryptr = new Cryptr(process.env.QR_SECRET_KEY, { encoding: 'base64' })
 
 export function encryptQR(string: string){
-    return cryptr.encrypt(string)
+    const cryptr = initCryptr()
+    const encodedString = cryptr.encrypt(string)
+    return encodedString
 }
 
 export function decryptQR(string: string){
-    return cryptr.decrypt(string)
+    const cryptr = initCryptr()
+    try {
+        const decodedString = cryptr.decrypt(string)
+        return decodedString
+    } catch(e){
+        return 'INVALID QR CODE FORMAT'
+    }
 }
